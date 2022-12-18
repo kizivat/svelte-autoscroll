28bed8c956bdf5771c0b43ec1189087409549505
David Kizivat <david.kizivat@nettle.ai>
2022-12-18 01:33:37 +0100

    feat: add `pauseOnUserScroll` option as argument
    The `pauseOnUserScroll` option is a boolean option. If it's set to `true` the autoscroll behavior is turned off and the scroll position remains at whatever position the user scrolls to. After the user scrolls to the bottom of the container, the autoscroll behavior is re-engaged.

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
+type AutoscrollOptions = {
+	pauseOnUserScroll?: boolean;
+};
+
 export default function autoscroll(
 	node: HTMLElement,
-	options: ScrollOptions = { behavior: 'smooth' }
+	options: AutoscrollOptions & ScrollOptions = {
+		behavior: 'smooth',
+	}
 ) {
 	const forbiddenOverflows = ['visible', 'hidden'];
 	if (
@@ -12,11 +18,15 @@ export default function autoscroll(
 		);
 	}
 
+	const { pauseOnUserScroll: _, ...scrollOptions } = {
+		behavior: 'smooth' as const,
+		...options,
+	};
 	const scroll = () => {
 		node.scrollTo({
 			top: node.scrollHeight,
 			left: node.scrollWidth,
-			...options,
+			...scrollOptions,
 		});
 	};
 
@@ -25,25 +35,55 @@ export default function autoscroll(
 		scroll();
 	});
 
-	// for when children
+	// for when children are added or removed/subtree changes
 	const mutationObserver = new MutationObserver(_ => {
 		scroll();
 	});
 
-	// observe size of all children
-	for (const child of node.children) {
-		resizeObserver.observe(child);
+	const observeAll = () => {
+		// observe size of all children
+		for (const child of node.children) {
+			resizeObserver.observe(child);
+		}
+
+		mutationObserver.observe(node, { childList: true, subtree: true });
+	};
+
+	observeAll();
+
+	const handleScroll = () => {
+		if (node.scrollTop + node.clientHeight < node.scrollHeight) {
+			mutationObserver.disconnect();
+			resizeObserver.disconnect();
+		} else {
+			observeAll();
+		}
+	};
+
+	if (options.pauseOnUserScroll) {
+		node.addEventListener('scroll', handleScroll);
 	}
 
-	mutationObserver.observe(node, { childList: true, subtree: true });
-
 	return {
+		update({ pauseOnUserScroll, behavior }: AutoscrollOptions & ScrollOptions) {
+			if (pauseOnUserScroll) {
+				node.addEventListener('scroll', handleScroll);
+				mutationObserver.disconnect();
+				resizeObserver.disconnect();
+			} else {
+				node.removeEventListener('scroll', handleScroll);
+				observeAll();
+			}
+		},
 		destroy() {
 			if (mutationObserver) {
 				mutationObserver.disconnect();
 			}
 			if (resizeObserver) {
 				resizeObserver.disconnect();
+			}
+			if (options.pauseOnUserScroll) {
+				node.removeEventListener('scroll', handleScroll);
 			}
 		},
 	};
